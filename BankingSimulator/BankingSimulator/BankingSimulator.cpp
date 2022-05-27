4d296ddd4572c53dfebdae36ec2a7c3671771e4d
Daniel Ryan <dryan2@ncsu.edu>
2022-05-27 09:51:12 -0400

    Initial commit.  Setup solution

---
+++ BankingSimulator/BankingSimulator/BankingSimulator.cpp
@@ -0,0 +1,11 @@
+// BankingSimulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
